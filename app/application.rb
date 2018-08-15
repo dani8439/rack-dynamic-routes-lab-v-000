48a2b7eb44930e250ec9897218f72195813ca70e
Dani Schuhman <dani8439@aol.com>
2018-08-15 17:53:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last #turn /items/Pears into Pears
-      item = @@items.find{|i| i.name == item_name}
+      if item = @@items.find{|i| i.name == item_name}
 
       resp.write item.price
       resp.status = 200
@@ -15,7 +15,7 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
-    
+
     resp.finish
   end
 end
