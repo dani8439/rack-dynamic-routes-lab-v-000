925020cc0a80dca5f9d2488d7e6ebe610b4db194
Dani Schuhman <dani8439@aol.com>
2018-08-15 17:48:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,16 +2,16 @@ class Application
   @@items = [Item.new("Figs",3.42),Item.new("Pears",0.99)]
 
   def call(env)
-    resp = Rack::Response.new 
+    resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items/").last #turn /items/Pears into Pears 
+      item_name = req.path.split("/items/").last #turn /items/Pears into Pears
       item = @@items.find{|i| i.name == item_name}
 
-      resp.write item.
-    end 
+      resp.write item.price
+    end
 
-    resp.finish 
+    resp.finish
   end
 end
