0c3e02054a80371597873542d3d1049672b7fd43
Dani Schuhman <dani8439@aol.com>
2018-08-15 17:51:20 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,8 +12,8 @@ class Application
       resp.write item.price
       resp.status = 200
     else
-      resp.write "Route not found"
-      resp.status = 404
+      resp.write "Item not found"
+      resp.status = 400
     end
 
     resp.finish
