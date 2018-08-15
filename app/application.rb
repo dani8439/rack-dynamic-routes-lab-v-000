ac211530cd9c51c91eb437574c7c969d1a1b70ef
Dani Schuhman <dani8439@aol.com>
2018-08-15 17:49:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
       item = @@items.find{|i| i.name == item_name}
 
       resp.write item.price
+      resp.status = 200
     else
       resp.write "Route not found"
       resp.status = 400
